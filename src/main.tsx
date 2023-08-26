98fb121881c438e489d27563817c1672ed996348
Flysoft <flysoftbeta@outlook.com>
2023-08-26 22:13:33 +0800

    feat: 支持加载 React DevTools

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import App from "./App";
 import "./styles.css";
 import ReactDOM from "react-dom/client";
 
-export const isProduction = import.meta?.env?.PROD || false;
+export const isProduction = import.meta.env.PROD || false;
+export const isDevelopment = import.meta.env.DEV || false;
 
 window.addEventListener("contextmenu", (event) => event.preventDefault());
 
